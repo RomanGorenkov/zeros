da8bb8451e82b0a263776ac82d9452c0e556003d
RomanGorenkov <roman.gorenkov.one@gmail.com>
2019-09-25 15:20:30 +0300

    final commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function zeros(expression) {
   // your solution
+  let splitArray = expression.split('*');
+  let twoCount = 0;
+  let fiveCount = 0;
+  splitArray.forEach(element => {
+
+      let [value, counter] = [+(element.split('!')[0]), (element.length - element.indexOf('!'))];
+
+      for(; 0 < value ; value -= counter) {
+        let source = value;
+        while (source % 5 == 0) {
+            fiveCount++;
+            source /= 5;
+        }
+        
+        source = value;
+        
+        while (source % 2 == 0) {
+            twoCount++;
+            source /= 2;
+        }
+    }
+  });
+  
+  return    twoCount > fiveCount ? fiveCount : twoCount;
 }
